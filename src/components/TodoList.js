e387507d7ae5b1d72d70bbeefe8a59db8b428b73
Bhuvaneshwaran <125134187+bhuvan243@users.noreply.github.com>
2024-02-14 05:37:59 +0000

    fourth

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,38 +1,22 @@
-// import React from "react";
+import React from "react";
 
-// const TodoList = ({todos, handleComplete}) => {
+const TodoList = ({todos, handleComplete}) => {
 
-//     return (
-//         <div>
-//             <h3>Child Component</h3>
-//             <ul>
-//                 {
-//                     todos.map((todo, index) => {
-//                         return (
-//                             <li key={index}>{todo.title} {!todo.isComplete && <button onClick={() => handleComplete(todo.id)}>Complete</button>}</li>
-//                         )
-//                     })
-//                 }
-//             </ul>
-//         </div>
-//     )
-// }
-
-// export default TodoList;
-
-import React from 'react';
-
-const TodoList = ({ todos, onComplete }) => {
-  return (
-    <ul>
-      {todos.map((todo) => (
-        <li key={todo.id}>
-          <span>{todo.text}</span>
-          <button onClick={() => onComplete(todo.id)}>Complete</button>
-        </li>
-      ))}
-    </ul>
-  );
-};
+    return (
+        <div>
+            <h3>Child Component</h3>
+            <ul>
+                {
+                    todos.map((todo, index) => {
+                        return (
+                            <li key={index}>{todo.title} {!todo.isComplete && <button onClick={() => handleComplete(todo.id)}>Complete</button>}</li>
+                        )
+                    })
+                }
+            </ul>
+        </div>
+    )
+}
 
 export default TodoList;
+
